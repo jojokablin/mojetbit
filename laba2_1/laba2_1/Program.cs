617a508cac4bc83ca7dc82cb5ac45961b9750451
jojokabin <egorushkanutslove@gmail.com>
2019-02-20 08:51:39 +0700

    first master

---
+++ laba2_1/laba2_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите А: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            //enter sigh
+
+            //int c = a + b;
+            //int d = a - b;
+            //int e = a * b;
+            //int f = a / b;
+
+            Console.WriteLine("Результат сложения {0}", c);
+            Console.ReadKey();
+        }
+    }
+}
